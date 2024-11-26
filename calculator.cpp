aa2f057af8645a6633af9cd0b4dc105761fa91e8
Bara <baramrayyan@gmail.com>
2024-11-26 16:37:47 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -34,10 +34,16 @@ int main()
                 break;
             case -1:
                 return 0;
+            case -2:
+                x = newValue;
+                cout << "Enter the other number: ";
+                cin >> y;
+                cout << endl << "Choose from the menu: \n [1] + \n [2] - \n [3] * \n [4] / \n";
+                continue;
             default:
                 cout << "A number from 1 to 4 only";
         }
         cout << endl
-             << "Choose again (put -1 to exit): ";
+             << "Choose again (put -1 to exit and -2 to redo the calculator with the new value): ";
     }
 }
